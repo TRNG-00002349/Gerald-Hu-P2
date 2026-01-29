e91b3104a7b08f79bc9a0d24900d8c9b60b4ec0f
Piezoelectric <23746623+Piezoelectric@users.noreply.github.com>
2026-01-28 23:36:02 -0600

    use CrudRepository and override default behavior

--- backend/src/main/java/com/revature/backend/users/UserRepository.java
+++ backend/src/main/java/com/revature/backend/users/UserRepository.java
@@ -1,15 +1,22 @@
 package com.revature.backend.users;
 
-import jakarta.persistence.Id;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@RepositoryRestResource(collectionResourceRel = "users", path = "users")
-public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface UserRepository extends CrudRepository<User, Integer> {
 
-	User findById(@PathVariable Integer id);
+	@Query("select u from User u where u.deleted = FALSE")
+	List<User> findAll();
+
+	Optional<User> findById(@PathVariable Integer id);
+	// TODO: find users that AREN'T deleted
 
 	// List<User> findByUsername(@Param("username") String name);
 }
