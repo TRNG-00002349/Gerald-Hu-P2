ee898b4d1dc787d67516b0a49d06894172c1d3bf
Piezoelectric <23746623+Piezoelectric@users.noreply.github.com>
2026-02-04 12:38:59 -0600

    feature: user can log in

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { ApiClient, ApiResponse } from '../api-client';
+import { FormsModule } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  apiClient: ApiClient
+  username: string = ""
+  password: string = ""
 
+  loginResult: WritableSignal<String> = signal("")
+
+  constructor(apiClient: ApiClient) {
+    this.apiClient = apiClient
+  }
+
+  submitLoginForm(): void {
+    console.log("submitting login form...")
+    this.apiClient
+      .login(this.username, this.password)
+      .subscribe({
+        next: (body: ApiResponse) => {
+          console.log(body)
+          this.loginResult.set(`Logged in successfully!`)
+        },
+        error: (err: HttpErrorResponse) => {
+          console.log(err)
+          this.loginResult.set(`Couldn't log in: ${err?.error?.error}`)
+        }
+      })
+  }
 }
--- frontend/src/app/register/register.ts
+++ frontend/src/app/register/register.ts
@@ -3,12 +3,6 @@ import { FormsModule } from '@angular/forms';
 import { ApiClient, ApiResponse } from '../api-client';
 import { HttpErrorResponse, HttpResponse, HttpStatusCode } from '@angular/common/http';
 
-export enum RegistrationState {
-  NOT_STARTED = "NOT_STARTED",
-  SUCCESS = "SUCCESS",
-  FAILURE = "FAILURE"
-}
-
 @Component({
   selector: 'app-register',
   imports: [FormsModule],
@@ -22,7 +16,6 @@ export class Register {
   email: string = ""
 
   registrationResult: WritableSignal<String> = signal("")
-  registrationState: WritableSignal<RegistrationState> = signal(RegistrationState.NOT_STARTED)
 
   constructor(apiClient: ApiClient) {
     this.apiClient = apiClient
@@ -30,17 +23,17 @@ export class Register {
 
   submitRegistrationForm(): void {
     console.log("submitting registration form...")
-    this.apiClient.register(this.username, this.password, this.email).subscribe({
-      next: (body: ApiResponse) => {
-        console.log(body)
-        this.registrationResult.set(`Created! Your user ID is ${body.id}`)
-        this.registrationState.set(RegistrationState.SUCCESS)
-      },
-      error: (err: HttpErrorResponse) => {
-        console.log(err)
-        this.registrationResult.set(err?.error?.error)
-        this.registrationState.set(RegistrationState.FAILURE)
-      }
-    })
+    this.apiClient
+      .register(this.username, this.password, this.email)
+      .subscribe({
+        next: (body: ApiResponse) => {
+          console.log(body)
+          this.registrationResult.set(`Created! Your user ID is ${body.id}`)
+        },
+        error: (err: HttpErrorResponse) => {
+          console.log(err)
+          this.registrationResult.set(`Couldn't create user: ${err?.error?.error}`)
+        }
+      })
   }
 }
